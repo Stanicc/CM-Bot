7dea8f0eac2f297677402f694d732555c49bed4f
Stanicc <stanicmucierres@gmail.com>
2020-08-29 19:38:20 -0300

    📮 Starting the ticket system

--- src/main/kotlin/stanic/cmbot/guild/ticket/TicketManager.kt
+++ src/main/kotlin/stanic/cmbot/guild/ticket/TicketManager.kt
@@ -1,7 +1,12 @@
 package stanic.cmbot.guild.ticket
 
+import stanic.cmbot.guild.model.Guild
+import stanic.cmbot.guild.ticket.model.Ticket
+
 class TicketManager {
 
-
+    fun getTicketByID(guild: Guild, id: String): Ticket? = guild.tickets
+        .filter { it.id == id }
+        .getOrNull(0)
 
 }
